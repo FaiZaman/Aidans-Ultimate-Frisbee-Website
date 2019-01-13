7192570903c6922952fd43c0104700511d373e9d
faizzaman <faiz-uz.zaman@durham.ac.uk>
2019-01-13 16:57:58 +0000

    added htm for adding people, and started with json

--- js/app.js
+++ js/app.js
@@ -3,15 +3,26 @@
 /* jshint node: true */
 "use strict";
 
+const path = require('path');
 const express = require('express');
 const app = express();
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname + '/..')));
 
 app.get('/', (req, res) => {
-   res.send('GET route on things.');
+   res.sendStatus(200);
+   res.end();
 });
 
 app.post('/', (req, res) => {
    res.send('POST route on things.');
 });
+
+const requestDir = path.join(__dirname + '/../json/data.json');
+const request = new XMLHttpRequest();
+request.open("GET", requestDir);
 
 module.exports = app;
--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
 /* jshint -W097 */
-/*jshint esversion: 6 */
+/* jshint esversion: 6 */
 /* jshint node: true */
+/* jshint browser: true */
 "use strict";
 
 const app = require('./app.js');
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${ port }`));
+app.listen(port, () => console.log(`Server running at localhost:${ port }`));
+
+function openForm() {
+  if (document.getElementById("add-member").style.display != "block"){
+    document.getElementById("add-member").style.display = "block";
+  }
+  else {
+  document.getElementById("add-member").style.display = "none";
+  }
+}
+
+function closeForm() {
+  document.getElementById("add-member").style.display = "none";
+}
